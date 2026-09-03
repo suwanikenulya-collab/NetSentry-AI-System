595f4d468c0f79296c8cfc59f55dad860898dc55
Suwani Kenulya <suwanikenulya@gmail.com>
2026-09-03 05:38:14 +0530

    feat(datasets): define server validation result types

--- frontend/src/features/datasets/types/dataset.types.ts
+++ frontend/src/features/datasets/types/dataset.types.ts
@@ -10,3 +10,19 @@ export interface DatasetValidationResult {
   detectedColumns: string[];
   missingColumns: string[];
 }
+
+export interface DatasetValidationApiResponse {
+  filename: string;
+  is_valid: boolean;
+  row_count: number;
+  detected_columns: string[];
+  missing_columns: string[];
+  message: string;
+}
+
+export interface DatasetServerValidationResult
+  extends DatasetValidationResult {
+  filename: string;
+  rowCount: number;
+  message: string;
+}
